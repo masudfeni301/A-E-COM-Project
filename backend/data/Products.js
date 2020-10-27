803480afe5e17c8dd5d9181107f349048a3032fc
unknown <masudfeni302@gmail.com>
2020-10-27 23:23:18 +0600

    model added

--- backend/data/Products.js
+++ backend/data/Products.js
@@ -1,6 +1,5 @@
 const products = [
-    {_id:'1',
-    name:'Conion Rice Cooker BE 28B60',
+    {name:'Conion Rice Cooker BE 28B60',
     image:'/images/rice-cook-1.jpg',
     description:"Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book",
     brand:'Canon',
@@ -8,8 +7,8 @@ const products = [
     countInStrock:5,
     rating:1.5,
     numReviews:4},
-    {_id:'2',
-    name:'Philips Rice Cooker HD3027',
+
+    {name:'Philips Rice Cooker HD3027',
     image:'/images/rice-cook-2.jpg',
     description:"Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book",
     brand:'Philips',
@@ -17,8 +16,8 @@ const products = [
     countInStrock:6,
     rating:3.5,
     numReviews:4},
-    {_id:'3',
-    name:'Walton Rice Cooker WRC-CGA22D',
+
+    {name:'Walton Rice Cooker WRC-CGA22D',
     image:'/images/rice-cook-3.jpg',
     description:"Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book",
     brand:'Walton',
@@ -26,8 +25,8 @@ const products = [
     countInStrock:3,
     rating:5,
     numReviews:8},
-    {_id:'4',
-    name:'Walton Rice Cooker WRC-P105 1.0 L',
+    
+    {name:'Walton Rice Cooker WRC-P105 1.0 L',
     image:'/images/rice-cook-4.jpg',
     description:"Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book",
     brand:'Walton',
