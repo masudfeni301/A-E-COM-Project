f4df28187bad3d382e8a4751d7edcfb72837c2fd
unknown <masudfeni302@gmail.com>
2020-10-27 07:43:37 +0600

    proudct added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {products} from "../Product"
+import {products} from "../Products"
 import {Row, Col} from "react-bootstrap"
+import Product from '../components/Product'
 
 const HomeScreen = () => {
     return (
         <>
+        <h1>Latest Product</h1>
         <Row>
             {products.map((product)=>(
                 <Col sm="12" md="6" lg="4" xl="3">
-                    <h3>{product.name}</h3>
+                    <Product key={product._id} product={product}/>
                 </Col>
             ))}
         </Row>
